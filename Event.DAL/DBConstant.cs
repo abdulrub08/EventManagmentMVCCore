8174f8daa6b5caa33c6833d6a778f41d73dbe43e
abdul.rub@torrens.edu.au <abdul.rub@torrens.edu.au>
2023-06-02 12:39:39 +0530

    After Architecure Creation

---
+++ Event.DAL/DBConstant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event.DAL
+{
+    internal class DBConstant
+    {
+        #region Alumni
+        public const string UPSERTALUMNIORDERDETAIL = "PROC_WebApp_UpsertAlumniOrderDetail";
+        public const string UPSERTALUMNIORDER = "PROC_WebApp_UpsertAlumniOrder";
+        public const string DELETEALUMNIORDERDETAIL = "PROC_WebApp_DeleteAlumniOrderDetail";
+        public const string GETCOURSE = "PROC_WebApp_GetCourse";
+        public const string GETCOLLEGE = "PROC_WebApp_GetCollege";
+        public const string GETDOCUMENTPRICE = "PROC_WebApp_GetDocumentPrice";
+        public const string INSERTALUMNIORDER_ORDERDETAIL = "PROC_WebApp_InsertAlumniOrder_OrderDetail";
+        #endregion
+    }
+}
---
+++ Event.DOM/BookingDetailTemp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Event.DOM
+{
+    public class BookingDetailTemp
+    {
+        public int BookingID { get; set; }
+        public string BookingNo { get; set; }
+        public string BookingDate { get; set; }
+        public string Createdby { get; set; }
+        public string CreatedDate { get; set; }
+        public string BookingApproval { get; set; }
+        public string BookingApprovalDate { get; set; }
+    }
+}
