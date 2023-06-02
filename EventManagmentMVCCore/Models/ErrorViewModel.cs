cd5638542d12dc670771ff612b61e51a3eb83ab8
abdul.rub@torrens.edu.au <abdul.rub@torrens.edu.au>
2023-06-02 10:59:45 +0530

    Created First time

---
+++ EventManagmentMVCCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EventManagmentMVCCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
